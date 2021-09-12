90d2f65a0d0dd82f2529bbb304e522f4594211e8
A M Fahad Hassan <imfahadhassan@gmail.com>
2021-09-12 07:40:13 -0500

    Create maze.js

---
+++ js/maze.js
@@ -0,0 +1,45 @@
+var success = true;
+$(function () {
+  $("#start").click(reset);
+  $("#maze").hover(
+    function(){
+      $("#maze div.boundary").on();
+      $("#end").on();
+    },
+    function(){
+      $("#maze div.boundary").off();
+      $("#end").off();
+    }
+    );
+});
+
+  var hoverStatus = $("#status").text();
+  function red() {
+    success = false;
+    hoverStatus = "Sorry, you lost";
+    $("#status").html(hoverStatus).css("color", "red");
+    $(".boundary").each(function () {
+      $(this).addClass("youlose");
+    });
+  }
+
+  function end() {
+    hoverStatus = "Game End!";
+    $("#status").html(hoverStatus).css("color", "blue");
+    let currentStatus = success == true ? "You win! :]" : "Sorry, you lost! :[";
+    alert(currentStatus);
+    $(this).off();
+    $("#maze div.boundary").off();
+    return true;
+  }
+
+  function reset() {
+    success = true;
+    hoverStatus = "Game Started!";
+    $("#status").html(hoverStatus).css("color", "green");
+    $(".boundary").each(function () {
+      $(this).removeClass("youlose");
+    });
+    $("#maze div.boundary").on("mouseover", red);
+    $("#end").on("mouseover", end);
+  }
