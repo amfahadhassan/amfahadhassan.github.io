e28605e2d7b229041a55a9123b49287b3bbc7df0
A M Fahad Hassan <imfahadhassan@gmail.com>
2021-09-18 06:36:21 -0500

    Update server.js

--- SimpleCalculator/server.js
+++ SimpleCalculator/server.js
@@ -7,15 +7,15 @@ var fs = require("fs");
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var ext = path.extname(q.pathname);
-    if (ext === ".css") {
+    if (ext == ".css") {
         res.writeHead(200, { "Content-Type": "text/css" });
         res.write(fs.readFileSync("style.css"), "utf-8");
-    } else if (q.pathname === "./cModule.js") {
+    } else if (q.pathname == "./cModule.js") {
         cModule.calc(req, res, q.query);
     } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(fs.readFileSync("index.html"), "utf-8");
     }
     res.end();
-}).listen(8081);
+}).listen(8080);
 
